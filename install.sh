5a621bd239d7362dd7768d994dfe6d3da91fb4c6
yaizuuuu <24151739+yaizuuuu@users.noreply.github.com>
2026-04-07 17:07:53 +0900

    log helper改善 & ファイルの存、確認を追加

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -u
+set -euo pipefail
 
 # 事前準備:
 # brewはあらかじめインストールしておく
@@ -10,22 +10,21 @@ set -u
 # colored log helpers
 
 info() {
-  echo -e "\033[1;32m[INFO]\033[0m \033[1m$1\033[0m"
+  echo -e "\033[1;32m[INFO]\033[0m \033[1m=== $1 ===\033[0m"
 }
 
 warn() {
-  echo -e "\033[1;33m[WARN]\033[0m \033[1m$1\033[0m"
+  echo -e "\033[1;33m[WARN]\033[0m \033[1m=== $1 ===\033[0m"
 }
 
 error() {
-  echo -e "\033[1;31m[ERROR]\033[0m \033[1m$1\033[0m" >&2
+  echo -e "\033[1;31m[ERROR]\033[0m \033[1m=== $1 ===\033[0m" >&2
 }
 
 if output=$(brew --version 2>/dev/null); then
-  info "=== already installed brew: ${output} ==="
+  info "already installed brew: ${output}"
 else
-  echo "brew の取得に失敗しました"
-  error "=== not installed brew ==="
+  error "not installed brew"
   exit 1
 fi
 
@@ -34,29 +33,42 @@ DOTFILES_DIR="${GHQ_DIR}/github.com/yaizuuuu/dotfiles"
 info "dotfiles dir: ${DOTFILES_DIR}"
 
 
-info "=== installing bundle from .Brewfile ==="
+info "installing bundle from .Brewfile"
 
 # google-japanese-imeが更新された場合、端末の再起動が必要な場合がある
+if [ -f "${HOME}/.Brewfile" ]; then
+  info "already existed ${HOME}/.Brewfile"
+else
+  warn "not yet existed ${HOME}/.Brewfile, fetching .Brewfile"
+  curl -fsSL https://raw.githubusercontent.com/yaizuuuu/dotfiles/HEAD/.Brewfile > $HOME/.Brewfile
+fi
 brew bundle --global
 
-if output=$(gh auth status 2> /dev/null); then
-  info "=== already login github:\n${output} ==="
+
+info "cloning dotfiles repo"
+
+if gh auth status 2> /dev/null; then
+  info "already login github"
 else
-  warn "=== not yet login github ==="
-  gh auth login
+  warn "not yet login github"
+  gh auth login -c -p https -w
 fi
 
+if [ -f "${HOME}/.gitconfig" ]; then
+  info "already existed ${HOME}/.gitconfig"
+else
+  warn "not yet existed ${HOME}/.gitconfig, fetching .gitconfig"
+  curl -fsSL https://raw.githubusercontent.com/yaizuuuu/dotfiles/HEAD/.gitconfig > $HOME/.gitconfig
+fi
 
-info "=== cloning dotfiles repo ==="
-
-ln -sf  $DOTFILES_DIR/.gitconfig ~/.gitconfig
 ghq get https://github.com/yaizuuuu/dotfiles
 
 
-info "=== creating links ==="
+info "creating links"
 
 ln -sf  $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -sf  $DOTFILES_DIR/.Brewfile ~/.Brewfile
+ln -sf  $DOTFILES_DIR/.gitconfig ~/.gitconfig
 
 mkdir -p ~/.config/ghostty
 mkdir -p ~/.config/git
@@ -74,6 +86,6 @@ ln -sf  $DOTFILES_DIR/.claude/notify.sh ~/.claude/notify.sh
 ln -sf  $DOTFILES_DIR/.claude/settings.json ~/.claude/settings.json
 
 
-info "=== installing safe-chain ==="
+info "installing safe-chain"
 
 curl -fsSL https://github.com/AikidoSec/safe-chain/releases/latest/download/install-safe-chain.sh | sh
